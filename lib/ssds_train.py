1a90f44b96585fe4b0950d7992a3dfd4799b67a9
zl <zengl65e@visionvera.com>
2018-10-18 11:43:10 +0800

    d

--- lib/ssds_train.py
+++ lib/ssds_train.py
@@ -36,7 +36,7 @@ class Solver(object):
         print('===> Loading data')
         self.train_loader = load_data(cfg.DATASET, 'train') if 'train' in cfg.PHASE else None
         self.eval_loader = load_data(cfg.DATASET, 'eval') if 'eval' in cfg.PHASE else None
-        self.test_loader = load_data(cfg.DATASET, 'test') if 'test' in cfg.PHASE else None
+     #   self.test_loader = load_data(cfg.DATASET, 'test') if 'test' in cfg.PHASE else None
         self.visualize_loader = load_data(cfg.DATASET, 'visualize') if 'visualize' in cfg.PHASE else None
 
         # Build model
