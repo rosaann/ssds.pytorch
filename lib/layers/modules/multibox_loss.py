71dae49f76b4166214b768d890701debd2284d4b
zl <zengl65e@visionvera.com>
2018-10-20 17:44:24 +0800

    d

--- lib/layers/modules/multibox_loss.py
+++ lib/layers/modules/multibox_loss.py
@@ -91,12 +91,9 @@ class MultiBoxLoss(nn.Module):
         #np.set_printoptions(threshold=np.inf)
         #print('conf_t  ',np.array( conf_t_v))
         #print('batch_conf ', batch_conf)
-        #loss_c = log_sum_exp(batch_conf) - batch_conf.gather(1, conf_t_v)#zz 下一句从别地考的
-        loss_c = log_sum_exp (batch_conf) - batch_conf.gather (0, conf_t.view (-1, 1))
-        np.set_printoptions(threshold=20)
-        # Hard Negative Mining
-        print('loss_c size ', loss_c.size(0))
-       # print('pos  ', pos)
+        loss_c = log_sum_exp(batch_conf) - batch_conf.gather(1, conf_t_v)#zz 下一句从别地考的
+        #loss_c = log_sum_exp (batch_conf) - batch_conf.gather (0, conf_t.view (-1, 1))
+        loss_c = loss_c.view(pos.size()[0], pos.size()[1]) #add line 
 
         loss_c[pos] = 0 # filter out pos boxes for now
         loss_c = loss_c.view(num, -1)
