a5f69daa725e6c90d88b496c320a9c15b48d7a47
zl <zengl65e@visionvera.com>
2018-10-24 10:53:45 +0800

    d

--- lib/layers/modules/multibox_loss.py
+++ lib/layers/modules/multibox_loss.py
@@ -82,12 +82,13 @@ class MultiBoxLoss(nn.Module):
         # Compute max conf across batch for hard negative mining
        # batch_conf = conf_data.view(-1, self.num_classes)
         batch_conf = conf_data.view(-1, num_priors) #zl
-        conf_t_v = conf_t.view(-1,1)
+        #conf_t_v = conf_t.view(-1,1)
+        conf_t_v = conf_t.view(-1,num_priors)#zl
         #np.set_printoptions(threshold=np.inf)
         #print('conf_t  ',np.array( conf_t_v))
         #print('batch_conf ', batch_conf)
-        loss_c = log_sum_exp(batch_conf) - batch_conf.gather(1, conf_t_v)#zz 下一句从别地考的
-        #loss_c = log_sum_exp (batch_conf) - batch_conf.gather (0, conf_t.view (-1, 1))
+        loss_c = log_sum_exp(batch_conf) - batch_conf.gather(1, conf_t_v)
+        #loss_c = log_sum_exp (batch_conf) - batch_conf.gather (0, conf_t.view (-1, 1))#zz 
         loss_c[pos] = 0 # filter out pos boxes for now
         loss_c = loss_c.view(num, -1)
         ###
