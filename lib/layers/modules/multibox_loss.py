10b77faa154ecb7e1eb1b241d2992d18dbb97e8d
zl <zengl65e@visionvera.com>
2018-10-24 11:29:11 +0800

    d

--- lib/layers/modules/multibox_loss.py
+++ lib/layers/modules/multibox_loss.py
@@ -87,6 +87,9 @@ class MultiBoxLoss(nn.Module):
         #np.set_printoptions(threshold=np.inf)
         #print('conf_t  ',np.array( conf_t_v))
         #print('batch_conf ', batch_conf)
+        print('batch_conf size ', batch_conf.size(0))
+        print('conf_t_v size ', conf_t_v.size(0))
+
         loss_c = log_sum_exp(batch_conf) - batch_conf.gather(1, conf_t_v)
         #loss_c = log_sum_exp (batch_conf) - batch_conf.gather (0, conf_t.view (-1, 1))#zz 
         loss_c[pos] = 0 # filter out pos boxes for now
