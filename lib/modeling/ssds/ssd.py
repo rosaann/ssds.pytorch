8d908760eef3c53ea22aaf1e4acd8074d903b7b4
zl <zengl65e@visionvera.com>
2018-10-28 21:38:00 +0800

    d

--- lib/modeling/ssds/ssd.py
+++ lib/modeling/ssds/ssd.py
@@ -101,6 +101,7 @@ class SSD(nn.Module):
                 conf.view(conf.size(0), -1, self.num_classes),
                 #conf
             )
+        print('out put shape', output.shape)
         return output
 
 def add_extras(base, feature_layer, mbox, num_classes, version):
