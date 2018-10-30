2ec3f744d5d0f8158c8954df4f2540d08d8656d3
zl <zengl65e@visionvera.com>
2018-10-30 15:25:53 +0800

    d

--- lib/ssds_train.py
+++ lib/ssds_train.py
@@ -515,7 +515,7 @@ class Solver(object):
         # preproc.p = 0.6
 
         # preproc image & visualize preprocess prograss
-        images = Variable( self.train_loader.preproc(images, anno).unsqueeze(0), volatile=True)
+        images = Variable( preproc(images, anno).unsqueeze(0), volatile=True)
         if use_gpu:
             images = images.cuda()
 
