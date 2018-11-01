e4f76de5243beab3fb6e19d73bdd77a2ce862c18
zl <zengl65e@visionvera.com>
2018-11-01 16:47:23 +0800

    d

--- lib/ssds_train.py
+++ lib/ssds_train.py
@@ -472,7 +472,7 @@ class Solver(object):
                             box *= scale
                             box = np.append(box, score)
                             cls_dets.append(box)
-                            if score > 0.4:
+                            if score > 0.3:
                                 #vis.images(this_img, win=1, opts={'title': 'Reals'})
                                 print('box ', box)
                                 print('score ', score)
