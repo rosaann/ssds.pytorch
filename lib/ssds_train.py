46bbbb9c984e5bdb445c0de6533cc6c92d8fa58e
zl <zengl65e@visionvera.com>
2018-11-03 17:23:09 +0800

    d

--- lib/ssds_train.py
+++ lib/ssds_train.py
@@ -513,8 +513,9 @@ class Solver(object):
                     
     def showTestResult(self,writer, img_dir, cls_dets):
         image_show = cv2.imread(img_dir, cv2.IMREAD_COLOR)
+        cls_dets *= 768
         for box in cls_dets:
-            dets =  box*float(768)
+          #  dets =  box*float(768)
             xs = dets[ 0]
             ys = dets[ 1]
             x2s = dets[ 2] 
