f456162a6a98dee9a4accb0ee129403d4e8ccf41
zl <zengl65e@visionvera.com>
2018-11-03 15:44:20 +0800

    d

--- lib/ssds_train.py
+++ lib/ssds_train.py
@@ -465,7 +465,7 @@ class Solver(object):
                         images = Variable(dataset.preproc(img)[0].unsqueeze(0), volatile=True)
 
                 _t.tic()
-                if check_i == 0:
+                if check_i == 1:
                     vis.images(images[0], win=2, opts={'title': 'Reals'})
                     self.visTest(model, images[0].unsqueeze(0), self.priorbox, self.writer, 1, use_gpu)
                     
@@ -473,7 +473,7 @@ class Solver(object):
                 detections = detector.forward(out)
                 time = _t.toc()
                 for im ,this_img in enumerate( images):
-                  if check_i == 0:
+                  if check_i == 1:
                       if im == 0:
                           print('de ', detections[im])
                          # return
