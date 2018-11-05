f5028fd3557f08e36cda7e634dabfbf62ec6fa57
zl <zengl65e@visionvera.com>
2018-11-05 13:05:52 +0800

    d

--- lib/ssds_train.py
+++ lib/ssds_train.py
@@ -566,6 +566,19 @@ class Solver(object):
             if(box1_point_4[1] - box2_point_1[1]) * (box2_point_2[1] - box1_point_4[1]) > 0:
                 return True
             
+        if (box1_point_1[0] - box2_point_3[0]) * (box2_point_4[0] - box1_point_1[0]) > 0:
+            if(box1_point_1[1] - box2_point_3[1]) * (box2_point_4[1] - box1_point_1[1]) > 0:
+                return True
+        if (box1_point_2[0] - box2_point_3[0]) * (box2_point_4[0] - box1_point_2[0]) > 0:
+            if(box1_point_2[1] - box2_point_1[1]) * (box2_point_2[1] - box1_point_2[1]) > 0:
+                return True
+        if (box1_point_3[0] - box2_point_3[0]) * (box2_point_4[0] - box1_point_3[0]) > 0:
+            if(box1_point_3[1] - box2_point_3[1]) * (box2_point_4[1] - box1_point_3[1]) > 0:
+                return True
+        if (box1_point_4[0] - box2_point_3[0]) * (box2_point_4[0] - box1_point_4[0]) > 0:
+            if(box1_point_4[1] - box2_point_3[1]) * (box2_point_4[1] - box1_point_4[1]) > 0:
+                return True
+            
         if (box2_point_1[0] - box1_point_1[0]) * (box1_point_2[0] - box2_point_1[0]) > 0:
             if(box2_point_1[1] - box1_point_1[1]) * (box1_point_2[1] - box2_point_1[1]) > 0:
                 return True
