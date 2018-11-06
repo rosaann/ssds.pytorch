ba6f5dc6b1a1f759fec16f3228741a35cc158f54
zl <zengl65e@visionvera.com>
2018-11-06 12:54:48 +0800

    d

--- lib/ssds_train.py
+++ lib/ssds_train.py
@@ -565,10 +565,10 @@ class Solver(object):
            
            img_bk = cv2.cvtColor(image_for_cut,cv2.COLOR_BGR2GRAY)
            img_bk[0:767, 0:767] = 0
-          # img_bk[int (ovlap_box[1]):int (ovlap_box[3]), int(ovlap_box[0]):int(ovlap_box[2])] = th2[0:int (ovlap_box[3]) - int (ovlap_box[1]),0 : int (ovlap_box[2]) - int (ovlap_box[0])]
-           contours = cv2.findContours(th2,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+           img_bk[int (ovlap_box[1]):int (ovlap_box[3]), int(ovlap_box[0]):int(ovlap_box[2])] = th2[0:int (ovlap_box[3]) - int (ovlap_box[1]),0 : int (ovlap_box[2]) - int (ovlap_box[0])]
+         #  contours = cv2.findContours(th2,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
            print('cont ',contours)
-           cv2.drawContours(img_bk, contours[0], -1, 1, 3)
+          # cv2.drawContours(img_bk, contours[0], -1, 255, 3)
            
            cv2.imwrite(os.path.join('./data/','{}_3.png'.format(i)), th2)
            cv2.imwrite(os.path.join('./data/','{}_4.png'.format(i)), img_bk)
