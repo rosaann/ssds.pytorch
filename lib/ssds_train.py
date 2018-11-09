b493a460b310266c27e9303c1b3900e337e646f0
zl <zengl65e@visionvera.com>
2018-11-09 11:35:02 +0800

    d

--- lib/ssds_train.py
+++ lib/ssds_train.py
@@ -292,7 +292,7 @@ class Solver(object):
     def train_epoch(self, model, data_loader, optimizer, criterion, writer, epoch, use_gpu):
         
 
-        epoch_size = int( len(data_loader) / self.cfg.DATASET.TRAIN_BATCH_SIZE)
+        epoch_size =  len(data_loader) 
         batch_iterator = iter(data_loader)
 
         loc_loss = 0
